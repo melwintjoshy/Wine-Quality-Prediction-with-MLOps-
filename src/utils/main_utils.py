d3fc86c6aa4a5eadd149e73966848b1e746ffa3a
melwintjoshy <melwin465@gmail.com>
2025-06-20 19:34:17 +0530

    updated utils

--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -22,6 +22,13 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise e
 
 @ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+@ensure_annotations
 def save_json(path: Path, data: dict):
     with open(path, 'w') as f:
         json.dump(data, f, indent = 4)
@@ -46,6 +53,6 @@ def save_bin(data: Any, path: Path):
 def load_bin(path: Path):
     data = joblib.load(path)
     logger.info(f"binary file loaded from: {path}")
-    return data
+    return data 
 
 
