01ef4e33a0896d93399ddd7797fe49d33ecc8854
melwintjoshy <melwin465@gmail.com>
2025-06-21 13:24:22 +0530

    added get_validation_config_fn

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,6 +1,6 @@
 from src.constants import *
 from src.utils.main_utils import read_yaml, create_directories
-from src.entity.config_entity import DataIngestionConfig
+from src.entity.config_entity import DataIngestionConfig, DataValidationConfig
 
 class ConfigurationManager:
     def __init__(self,
@@ -13,13 +13,28 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
-        def get_data_ingestion_config(self)-> DataIngestionConfig:
-            config=self.config.data_ingestion
-            create_directories([config.root_dir])
+    def get_data_ingestion_config(self)-> DataIngestionConfig:
+        config=self.config.data_ingestion
+        create_directories([config.root_dir])
 
-            data_ingestion_config=DataIngestionConfig(
-                root_dir=config.root_dir,
-                source_URL=config.source_URL,
-                local_data_file=config.local_data_file
-            )
-            return data_ingestion_config
+        data_ingestion_config=DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file
+        )
+        return data_ingestion_config
+    
+    def get_data_validation_config(self)-> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config=DataValidationConfig(
+            root_dir = config.root_dir,
+            STATUS_FILE = config.STATUS_FILE,
+            local_data_file=config.local_data_file,
+            all_schema = schema
+        )
+        return data_validation_config
+
