6ea44cbd10635bf22784ec88d9abdc40b5100ee7
melwintjoshy <melwin465@gmail.com>
2025-06-21 13:25:10 +0530

    added data validation config entity

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -6,3 +6,10 @@ class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path 
+
+@dataclass
+class DataValidationConfig:
+    root_dir: Path
+    local_data_file: Path
+    STATUS_FILE: str 
+    all_schema: dict 
