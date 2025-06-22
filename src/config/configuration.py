35b10127da798b7c21ed27be391a0a60dd3b3b2d
melwintjoshy <melwin465@gmail.com>
2025-06-22 14:26:51 +0530

    added get_model_trainer fn

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,6 +1,6 @@
 from src.constants import *
 from src.utils.main_utils import read_yaml, create_directories
-from src.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from src.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 
 class ConfigurationManager:
     def __init__(self,
@@ -48,3 +48,21 @@ class ConfigurationManager:
             data_path= config.data_path
         )
         return data_transformation_config
+
+    def get_model_trainer_config(self)-> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir = config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+        )
+        return model_trainer_config
