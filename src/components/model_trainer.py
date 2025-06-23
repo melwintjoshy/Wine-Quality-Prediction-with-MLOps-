b0a44c059c6e037ef8ff8eb093029141da03a8b9
melwintjoshy <melwin465@gmail.com>
2025-06-23 17:50:01 +0530

    updated

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -11,12 +11,9 @@ class ModelTrainer:
     
     def training(self):
         train_data = pd.read_csv(self.config.train_data_path)
-        test_data = pd.read_csv(self.config.test_data_path)
 
         X_train = train_data.drop(self.config.target_column, axis = 1)
-        X_test  = test_data.drop(self.config.target_column, axis = 1)
         y_train = train_data[[self.config.target_column]]
-        y_test = test_data[[self.config.target_column]]
 
         lr = ElasticNet(alpha = self.config.alpha, l1_ratio=self.config.l1_ratio, random_state= 42)
         lr.fit(X_train, y_train)
