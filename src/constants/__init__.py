8410009e915d985c63c2bcda0300ff38934e9285
melwintjoshy <melwin465@gmail.com>
2025-06-23 18:53:06 +0530

    updated

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -3,3 +3,5 @@ from pathlib import Path
 CONFIG_FILE_PATH = Path("config/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
 SCHEMA_FILE_PATH = Path("schema.yaml")
+
+ML_FLOW_URI = "https://dagshub.com/melwintjoshy/Mlops-proj1.mlflow"
